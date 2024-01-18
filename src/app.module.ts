895e94eb45f91369e64ceac4a03da92a65e9cb03
shair1020 <ali99shair@gmail.com>
2024-01-18 12:37:50 +0500

    fix : user implementation basic

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from "./app.service";
 import { ProductModule } from "./product/product.module";
 import { MongooseModule } from "@nestjs/mongoose";
 import { MONGO_URI } from "./config";
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProductModule, MongooseModule.forRoot(MONGO_URI)],
+  imports: [ProductModule, MongooseModule.forRoot(MONGO_URI), UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/index.ts
+++ src/config/index.ts
@@ -4,3 +4,4 @@ dotenv.config();
 
 export const MONGO_URI = process.env.MONGO_URI;
 export const PORT = process.env.PORT;
+export const JWT_SECRET = process.env.JWT_SECRET;
